63066f3a50d9863949b436652af6902570acca64
A-Plus <87263769+a-plus-japan@users.noreply.github.com>
2021-07-30 17:21:17 +0900

    Update and rename config.sample.py to config.py

--- cognitive_face/tests/config.py
+++ cognitive_face/tests/config.py
@@ -10,11 +10,11 @@ Description: unittest configuration for Python SDK of the Cognitive Face API.
 """
 
 # Subscription Key for calling the Cognitive Face API.
-KEY = ''
+KEY = '3ddf4cc40c5a4b68911cb584bcc85695'
 
 # Base URL for calling the Cognitive Face API.
 # default is 'https://westus.api.cognitive.microsoft.com/face/v1.0/'
-BASE_URL = ''
+BASE_URL = 'https://a-plus-face-ide.cognitiveservices.azure.com/'
 
 # Time (in seconds) for sleep between each call to avoid exceeding quota.
 # Default to 3 as free subscription have limit of 20 calls per minute.
